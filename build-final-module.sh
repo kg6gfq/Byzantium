134b696c6c7b484403803af9875ac6da9ee68237
kg6gfq <darwin.enderson@gmail.com>
2012-05-27 06:26:22 +0000

    Fixed case issues.

--- build-final-module.sh
+++ build-final-module.sh
@@ -38,20 +38,20 @@ ln -s httpd www
 
 # We should build a controlpanel module to obviate these steps.
 mkdir -p /tmp/fakeroot/srv/controlpanel/graphs
-cp -rv ~guest/Byzantium/control_panel/srv/controlpanel/* /tmp/fakeroot/srv/controlpanel
+cp -rv ~guest/byzantium/control_panel/srv/controlpanel/* /tmp/fakeroot/srv/controlpanel
 mkdir -p /tmp/fakeroot/etc/controlpanel
-cp ~guest/Byzantium/control_panel/etc/controlpanel/* /tmp/fakeroot/etc/controlpanel
+cp ~guest/byzantium/control_panel/etc/controlpanel/* /tmp/fakeroot/etc/controlpanel
 mkdir -p /tmp/fakeroot/var/db/controlpanel
-cp -rv ~guest/Byzantium/control_panel/var/db/controlpanel/* /tmp/fakeroot/var/db/controlpanel
+cp -rv ~guest/byzantium/control_panel/var/db/controlpanel/* /tmp/fakeroot/var/db/controlpanel
 
 # We need to upgrade the version of Wicd to fix a bug, this is our own fix.
 # Note that when Poteus Linux updates to the latest stable version of wicd
 # this will become obsolete.
 mkdir -p /tmp/fakeroot/usr/share/wicd/cli
-cp ~guest/Byzantium/porteus/wicd/usr/share/wicd/cli/wicd-cli.py /tmp/fakeroot/usr/share/wicd/cli/
+cp ~guest/byzantium/porteus/wicd/usr/share/wicd/cli/wicd-cli.py /tmp/fakeroot/usr/share/wicd/cli/
 
 # Install our custom udev automount rules.
-cd ~guest/Byzantium/scripts
+cd ~guest/byzantium/scripts
 mkdir -p /tmp/fakeroot/etc/udev/rules.d
 cp 11-media-by-label-auto-mount.rules /tmp/fakeroot/etc/udev/rules.d
 
@@ -131,7 +131,7 @@ chmod 0750 /tmp/fakeroot/var/run/ngircd
 # Install the captive portal daemon.
 mkdir -p /tmp/fakeroot/srv/captiveportal
 mkdir -p /tmp/fakeroot/etc/captiveportal
-cd ~guest/Byzantium/captive_portal
+cd ~guest/byzantium/captive_portal
 cp captive_portal.py /tmp/fakeroot/usr/local/sbin
 cp captive-portal.sh /tmp/fakeroot/usr/local/sbin
 cp mop_up_dead_clients.py/tmp/fakeroot/usr/local/sbin
